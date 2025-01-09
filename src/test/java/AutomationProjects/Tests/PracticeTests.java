29a08a08b40c1bbcf2881e6ff0e40dd8ed1f562a
sreenath <sreenath.msc@gmail.com>
2025-01-09 18:23:35 +0530

    dev branch commit

--- src/test/java/AutomationProjects/Tests/PracticeTests.java
+++ src/test/java/AutomationProjects/Tests/PracticeTests.java
@@ -17,5 +17,19 @@ public class PracticeTests {
 		 System.out.println("This is test 2");
 		 System.out.println("This is from test 2");
 	 }
+	 
+	 @Test
+	 public void test3()
+	 {
+		 System.out.println("This is test 1");
+		 System.out.println("This is second");
+	 }
+	 
+	 @Test
+	 public void test4()
+	 {
+		 System.out.println("This is test 1");
+		 System.out.println("This is second");
+	 }
 
 }
